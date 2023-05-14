f55d94e895a8d3fae08085ec567f1714df78fcc3
Vijit Dhingra <vijitdhingra@gmail.com>
2023-05-13 22:23:30 -0700

    template

---
+++ simple_node/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {},
+};
