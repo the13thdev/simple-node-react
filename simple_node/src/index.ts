162be5b588063c722e62c1a145de6731fd2a5766
Vijit Dhingra <vijitdhingra@gmail.com>
2023-05-23 20:27:41 -0700

    add body parser

--- simple_node/src/index.ts
+++ simple_node/src/index.ts
@@ -1,8 +1,14 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
+  console.log('query params: ' + JSON.stringify(req.query));
   res.send('Hello World!');
 });
 
