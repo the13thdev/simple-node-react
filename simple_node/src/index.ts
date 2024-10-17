191a6566345e962be4c8d75f17716a01e3d2854d
Vijit Dhingra <vijitdhingra@gmail.com>
2024-10-16 23:11:00 -0700

    index.html

--- simple_node/src/index.ts
+++ simple_node/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
 
 const app = express();
 const port = 3000;
@@ -7,12 +8,19 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Serve static files from the "public" directory
+app.use(express.static(path.join(__dirname, './client')));
+
 app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './client/index.html'));
+});
+
+app.get('/a', (req, res) => {
   console.log('query params: ' + JSON.stringify(req.query));
   res.json({ queryParams: req.query });
 });
 
-app.post('/', (req, res) => {
+app.post('/a', (req, res) => {
   console.log('post data: ' + JSON.stringify(req.body));
   res.send({ postData: req.body });
 });
