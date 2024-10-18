0e82b0421232d0525b138cc9cd098685e5273ff9
Vijit Dhingra <vijitdhingra@gmail.com>
2024-10-17 22:48:46 -0700

    more stripe stuff

--- simple_node/src/index.ts
+++ simple_node/src/index.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import path from 'path';
 
+require('dotenv').config();
+const stripe = require('stripe')(process.env.STRIPE_TEST_API_KEY);
+
 const app = express();
 const port = 3000;
 
@@ -15,15 +18,47 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, './client/index.html'));
 });
 
-app.get('/a', (req, res) => {
+app.get('/', (req, res) => {
   console.log('query params: ' + JSON.stringify(req.query));
   res.json({ queryParams: req.query });
 });
 
-app.post('/a', (req, res) => {
-  console.log('post data: ' + JSON.stringify(req.body));
-  res.send({ postData: req.body });
+app.post('/reportUsage', async (req, res) => {
+  console.log('received /reportUsage: ' + JSON.stringify(req.body));
+
+  const usageCount = req.body.usageCount;
+  const meterEvent = await stripe.billing.meterEvents.create({
+    event_name: 'emoji_count',
+    payload: {
+      stripe_customer_id: 'cus_R2sq1hEyZIH1k4',
+      value: usageCount,
+    },
+  });
+
+  res.send({ success: true, meterEvent });
 });
+
+app.post(
+  '/webhook',
+  express.json({ type: 'application/json' }),
+  (request, response) => {
+    const event = request.body;
+
+    // Handle the event
+    switch (event.type) {
+      case 'billing.alert.triggered':
+        console.log('billing alert received');
+        console.log({ event });
+        break;
+
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a response to acknowledge receipt of the event
+    response.json({ received: true });
+  }
+);
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
